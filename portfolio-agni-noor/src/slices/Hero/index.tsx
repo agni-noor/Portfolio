85dbd9730659919676638f6a4ea32142145b58ec
Agni Noor <agninoor04@gmail.com>
2024-05-01 15:08:59 +0600

    minor changes to name and title animation

--- portfolio-agni-noor/src/slices/Hero/index.tsx
+++ portfolio-agni-noor/src/slices/Hero/index.tsx
@@ -34,6 +34,7 @@ useEffect(()=>{
     ease: "elastic.out(1,0.2)",
     duration: 1,
     transformOrigin: "left top",
+    delay:0.5,
     stagger:{
       each:0.1,
       from:"random",
@@ -86,7 +87,7 @@ useEffect(()=>{
         <span className="box text-slate-300">{renderLetters(slice.primary.first_name,"first")} </span>
         <span className="-mt-[.2em] block text-slate-500">{renderLetters(slice.primary.last_name, "last")}</span>
         </h1>
-        <span className="job-title block bg-gradient-to-tr from-yellow-500 via-yellow-200 to-yellow-500 bg-clip-text text-2xl font-bold uppercase tracking-[.2em] text-transparent opacity-100 md:text-4xl">{slice.primary.tag_line}</span>
+        <span className="job-title block bg-gradient-to-tr from-yellow-500 via-yellow-200 to-yellow-500 bg-clip-text text-2xl font-bold uppercase tracking-[.2em] text-transparent opacity-0 md:text-4xl">{slice.primary.tag_line}</span>
 
       </div>
     </div>
