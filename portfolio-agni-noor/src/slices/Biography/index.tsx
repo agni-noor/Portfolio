017f790a80e6f9e1c36cfd48be9c70ac54485030
Agni Noor <agninoor04@gmail.com>
2024-05-04 03:06:57 +0600

    prose typography added to about description

--- portfolio-agni-noor/src/slices/Biography/index.tsx
+++ portfolio-agni-noor/src/slices/Biography/index.tsx
@@ -1,5 +1,7 @@
+import Bounded from "@/components/Bounded";
+import Heading from "@/components/Heading";
 import { Content } from "@prismicio/client";
-import { SliceComponentProps } from "@prismicio/react";
+import { PrismicRichText, SliceComponentProps } from "@prismicio/react";
 
 /**
  * Props for `Biography`.
@@ -11,12 +13,20 @@ export type BiographyProps = SliceComponentProps<Content.BiographySlice>;
  */
 const Biography = ({ slice }: BiographyProps): JSX.Element => {
   return (
-    <section
+    <Bounded
       data-slice-type={slice.slice_type}
       data-slice-variation={slice.variation}
     >
-      Placeholder component for biography (variation: {slice.variation}) Slices
-    </section>
+      <div className="grid gap-x-8 gap-y-6 md:grid-cols-[2fr,1fr]">
+
+          <Heading as="h1" size="xl" className="col-start-1">
+            {slice.primary.heading}
+          </Heading>
+          <div className="prose prose-xl prose-slate prose-invert col-start-1">
+            <PrismicRichText field={slice.primary.description}/>
+          </div>
+      </div>
+    </Bounded>
   );
 };
 
