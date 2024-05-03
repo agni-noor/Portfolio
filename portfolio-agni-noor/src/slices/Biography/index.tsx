6c1eac6cb9114b7f8a7cfc69a73104d2e4567549
Agni Noor <agninoor04@gmail.com>
2024-05-04 03:26:14 +0600

    resume button link

--- portfolio-agni-noor/src/slices/Biography/index.tsx
+++ portfolio-agni-noor/src/slices/Biography/index.tsx
@@ -1,4 +1,5 @@
 import Bounded from "@/components/Bounded";
+import Button from "@/components/Button";
 import Heading from "@/components/Heading";
 import { Content } from "@prismicio/client";
 import { PrismicRichText, SliceComponentProps } from "@prismicio/react";
@@ -17,7 +18,7 @@ const Biography = ({ slice }: BiographyProps): JSX.Element => {
       data-slice-type={slice.slice_type}
       data-slice-variation={slice.variation}
     >
-      <div className="grid gap-x-8 gap-y-6 md:grid-cols-[2fr,1fr]">
+      <div className="grid1 gap-x-8 gap-y-6 md:grid-cols-[2fr,1fr]">
 
           <Heading as="h1" size="xl" className="col-start-1">
             {slice.primary.heading}
@@ -25,6 +26,7 @@ const Biography = ({ slice }: BiographyProps): JSX.Element => {
           <div className="prose prose-xl prose-slate prose-invert col-start-1">
             <PrismicRichText field={slice.primary.description}/>
           </div>
+          <Button linkField={slice.primary.button_link} label={slice.primary.button_text}/>
       </div>
     </Bounded>
   );
